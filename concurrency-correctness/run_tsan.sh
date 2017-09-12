95513b4175d534bc44d069c82a5332b59b5a20a6
Sanne <sanne-acc@posteo.net>
2017-09-12 11:21:42 +0200

    [dump] remove or move some unused files

--- concurrency-correctness/run_tsan.sh
+++ concurrency-correctness/run_tsan.sh
@@ -2,15 +2,9 @@
 
 sut=`echo $1`
 nr_threads=`echo $2`
-# llvm_base=`echo $3`
-
-llvm_base=/Users/sanne/Desktop/tools/llvm-repo
+llvm_base=`echo $3`
 
 here=`pwd .`
-
-#cd ${llvm_base}/build/projects/compiler-rt/lib/tsan
-#make
-#cd ${here}
 
 ${llvm_base}/build/bin/clang++ -std=c++14 -DNR_THREADS=${nr_threads} -O0 -g -fsanitize=thread ${sut} -o ${sut}_tsan
 ./${sut}_tsan
