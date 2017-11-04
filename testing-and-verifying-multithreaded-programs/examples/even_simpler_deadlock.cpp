bcedd68bfde85f24245ca7a41469cb1441069f86
Sanne <sanne-acc@posteo.net>
2017-11-04 17:49:59 +0100

    update

--- testing-and-verifying-multithreaded-programs/examples/even_simpler_deadlock.cpp
+++ testing-and-verifying-multithreaded-programs/examples/even_simpler_deadlock.cpp
@@ -10,12 +10,12 @@ int main()
 {
    std::mutex m1, m2;
    
-   std::thread thr1([&b, &m1, &m2]{
+   std::thread thr1([&m1, &m2]{
       std::lock_guard<std::mutex> lock1(m1);
       std::lock_guard<std::mutex> lock2(m2);
    });
    
-   std::thread thr2([&b, &m1, &m2]{
+   std::thread thr2([&m1, &m2]{
       std::lock_guard<std::mutex> lock2(m2);
       std::lock_guard<std::mutex> lock1(m1);
    });
