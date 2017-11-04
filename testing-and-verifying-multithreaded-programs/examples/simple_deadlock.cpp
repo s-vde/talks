86283297c6c3a7a20084f8f2f6c11198096c8c25
Sanne <sanne-acc@posteo.net>
2017-11-04 17:41:30 +0100

    fix compile error

--- testing-and-verifying-multithreaded-programs/examples/simple_deadlock.cpp
+++ testing-and-verifying-multithreaded-programs/examples/simple_deadlock.cpp
@@ -11,15 +11,15 @@ int main()
    std::mutex m1, m2;
    
    std::thread thr1([&b, &m1, &m2]{
-      std::lock_guard lock1(m1);
+      std::lock_guard<std::mutex> lock1(m1);
       while (!b) {}
-      std::lock_guard lock2(m2);
+      std::lock_guard<std::mutex> lock2(m2);
    });
    
    std::thread thr2([&b, &m1, &m2]{
-      std::lock_guard lock2(m2);
+      std::lock_guard<std::mutex> lock2(m2);
       b = true;
-      std::lock_guard lock1(m1);
+      std::lock_guard<std::mutex> lock1(m1);
    });
    
    thr1.join();
